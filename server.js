e31e9cd0b04998687a5ef7f1204b45a4ab6aca32
roy_benjamin <benji01560@gmail.com>
2016-08-25 20:13:54 +0000

    User stories done

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
 var express = require('express');
+var app = express();
+var useragent = require('express-useragent');
+var port = 8080;
+
+app.enable('trust proxy');
+app.use(useragent.express());
+
+app.get('/', function(req, res){
+    var m_ipaddress = req.ip;
+    var m_language = req.headers["accept-language"];
+    var m_software = req.useragent.os;
+        
+    // Selecting the main language:
+    var language = m_language.split(',');
+    language = language[0];
+  
+    console.log('The ip address is: ' + m_ipaddress);
+    console.log('The language is: ' + language);
+    console.log('The operating system is: ' + m_software);
+
+    res.json({
+        ipaddress: m_ipaddress,
+        language: language,
+        software: m_software
+    }); 
+});
+
+app.listen(process.env.PORT || port, function(){
+    console.log("App listening on port: " + port);
+});
